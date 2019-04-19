a1774044e1a375e04a8fec464a751dad0f96e4fe
Sandro Henrique <sandrohbn@gmail.com>
2019-04-19 19:11:55 -0300

    Criação classe usuário

--- class/Sql.php
+++ class/Sql.php
@@ -9,13 +9,13 @@
 			$this->conn = new PDO($SGBD.":host=".$Hospedeiro.";dbname=".$BD,$Usuario,$Senha);
 		}
 		*/
-		private function setParams($statment, $parameters = array()){
+		private function setParams($statement, $parameters = array()){
 			foreach ($parameters as $key => $value) {
-				$this->setParam($key, $value);
+				$this->setParam($statment, $key, $value);
 			}
 		}
-		private function setParam($statment, $key, $value){
-			$statment->bindParam($key, $value);
+		private function setParam($statement, $key, $value){
+			$statement->bindParam($key, $value);
 		}
 		public function query($rawQuery, $params = array()){
 			$stmt = $this->conn->prepare($rawQuery);
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 	require_once("config.php");
+	/*
 	$sql = new Sql();
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	echo json_encode($usuarios);
+	*/
+	//Usando a classe Usuario
+	$root = new Usuario();
+	$root->loadbyId(1);
+	echo $root;
 ?>
