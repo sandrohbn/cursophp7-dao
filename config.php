d8bb2bd167ae5d267adf80fb6167b9ec5ddf4fa5
Sandro Henrique <sandrohbn@gmail.com>
2019-04-19 17:21:22 -0300

    Inicio do projeto

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	//funcao anonima
+	spl_autoload_register(function($class_name){
+
+		$filename = "class".DIRECTORY_SEPARETOR.$class_name.".php";
+
+		if (file_exists($filename)){
+			require_once($filename);
+		}
+	});
+?>
